344e98c08b00cb91e8527808cfed8498eda8e24d
LyrinePoliarco <152787755+LyrinePoliarco@users.noreply.github.com>
2025-03-15 20:41:29 +0800

    copy-paste

--- app/adminData.tsx
+++ app/adminData.tsx
@@ -1,1 +1,9 @@
-git init
+import React from 'react';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { Button, colors, typography } from '../components';
+import Header from '../components/Header';
+import { useRouter } from 'expo-router';
+
+export default function AdminDataScreen() {
+  const router = useRouter();
+}
